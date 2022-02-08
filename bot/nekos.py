518d1b4c222d7b96485361f9ad506e727aba4568
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-08 18:15:03 +0700

    v3

--- bot/nekos.py
+++ bot/nekos.py
@@ -34,16 +34,14 @@ from telegram import (ParseMode, Update, InlineKeyboardMarkup,
 from bot.String import String
 
 delete_button = 'Delete'
-neko_MESSAGE = 'neko'
 
 def neko(update: Update, context: CallbackContext) -> None:
     msg = update.effective_message
     target = "neko"
     link = nekos.img(target)
-    reload = neko_MESSAGE
     link = link[23:],
     keyboard = [[InlineKeyboardButton(text="Send as file", callback_data=f"neko_callback, {link}, neko"),InlineKeyboardButton(text=f"Direct link",url=f"https://cdn.nekos.life/{link[0]}")]]
-    keyboard += [[InlineKeyboardButton(text=delete_button, callback_data=f"neko_delete, {msg.from_user.id}"),InlineKeyboardButton(text="Next", callback_data=f"neko_callback, {reload}, neko")]]
+    keyboard += [[InlineKeyboardButton(text=delete_button, callback_data=f"neko_delete, {msg.from_user.id}"),InlineKeyboardButton(text="Next", callback_data=f"neko_callback, neko"]]
     msg.reply_photo(f"https://cdn.nekos.life/{link[0]}",reply_markup=InlineKeyboardMarkup(keyboard))
     
 
