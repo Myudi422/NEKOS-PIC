f7d5e25c1d3a36ca899d6645434afd7c9fdd577d
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-09 09:19:54 +0700

    fixed

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ updater = Updater(CONFIG['bot_token'], use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler('pic', pic))
 updater.dispatcher.add_handler(CommandHandler('help1', chelp1))
-updater.dispatcher.add_handler(CallbackQueryHandler(help, pattern='zero_.*'))
+updater.dispatcher.add_handler(CallbackQueryHandler(help1, pattern='zero_.*'))
 updater.dispatcher.add_handler(CallbackQueryHandler(asfile, pattern='neko_.*'))
 
 updater.dispatcher.add_handler(CommandHandler("lewdkemo", lewdkemo))
