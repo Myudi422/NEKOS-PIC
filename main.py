94db4b6f6f5ec54eb2dcd8b1edae24b692e97faf
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-11 10:35:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ logger = logging.getLogger(__name__)
         
 updater = Updater(CONFIG['bot_token'], use_context=True)
 
+updater.dispatcher.add_handler(CommandHandler('image', upload))
 updater.dispatcher.add_handler(CommandHandler('pic', pic))
 updater.dispatcher.add_handler(CommandHandler('help1', chelp1))
 updater.dispatcher.add_handler(CallbackQueryHandler(help1, pattern='zero_.*'))
