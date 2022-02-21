7950d4696dc52da42b6855e2e1ca602c4aa68b89
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-21 19:48:55 +0700

    tes

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ logger = logging.getLogger(__name__)
 updater = Updater(CONFIG['bot_token'], use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler('pic', pic))
+updater.dispatcher.add_handler(CommandHandler('choose_photo', choose_photo))
 updater.dispatcher.add_handler(CommandHandler('re_scream', re_scream))
 updater.dispatcher.add_handler(CommandHandler('waifu',waifu))
 updater.dispatcher.add_handler(CommandHandler('help1', chelp1))
